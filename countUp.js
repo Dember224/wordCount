1cf32f40369615727ca625d4a99180c0e41a1059
Antoine Washington <antoine@Antoines-MacBook-Pro.local>
2020-10-07 20:04:08 -0400

    Provide word count for every article on the  Vox homepage

--- countUp.js
+++ countUp.js
@@ -1,18 +1,50 @@
 const request = require('request')
 const cheerio = require('cheerio')
-
-let website = "https://www.vox.com/2020/10/6/21504499/coronavirus-joint-chiefs-staff-coast-guard-test-positive"
-
-const getWebsite = request(website, function(error, response, body){
-
-  const $ = cheerio.load(body)
-  const word = $('p').text()
-
-  const wordArray = word.split(" ")
-
-  console.log(wordArray.length)
-});
+const async = require('async')
 
 
+let website = "https://www.vox.com"
 
-getWebsite
+function getLinks(site){
+  const getWebsite = request(site, function(error, response, body){
+
+    const $ = cheerio.load(body)
+    const hyperlinkTagArray = $('a').toArray().map(a =>{
+
+      return $(a).attr('href');
+    });
+
+    const filteredhyperlinkTagArray = hyperlinkTagArray.filter(
+      link => typeof link !== "undefined" && link.length > 29 && !link.includes("authors") && !link.includes("media") && !link.includes("book-club")
+    )
+
+    async.map(
+      filteredhyperlinkTagArray,
+      (url, results)=> {
+        request(url, function(error, response, html_body){
+          const $ = cheerio.load(html_body)
+          const word = $('p').text()
+
+          const wordArray = word.split(" ")
+
+          console.log(`Article: ${url} Word count: ${wordArray.length} `)
+        })
+      });
+    
+
+
+
+
+
+
+
+
+  });
+}
+
+
+
+
+
+
+getLinks(website)
