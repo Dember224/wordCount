ca19a1c50ef43f510eb66613b892bb6c14a2b7f1
Antoine Washington <antoine@Antoines-MacBook-Pro.local>
2020-10-08 15:42:54 -0400

    refactoring with options to perform with async

---
+++ autoInjectCount.js
@@ -0,0 +1,55 @@
+const request = require('request')
+const cheerio = require('cheerio')
+const async = require('async')
+
+let website = "https://www.vox.com"
+
+async.autoInject({
+  filtered_tags: function(callback){
+    request(website, function(error, response, body){
+      if(error){
+        return callback(error);
+      }
+
+      const $ = cheerio.load(body)
+      const hyperlinkTagArray = $('a').toArray().map(a =>{
+
+        return $(a).attr('href');
+      });
+
+      const filteredhyperlinkTagArray = hyperlinkTagArray.filter(
+        link => typeof link !== "undefined" && link.length > 29 && !link.includes("authors") && !link.includes("media") && !link.includes("book-club")
+      )
+      callback(null,filteredhyperlinkTagArray)
+    })
+  },
+  count_words:   function(filtered_tags, callback){
+      async.map(
+        filtered_tags,
+        (url, cb) =>{
+          request(url, function(error, response, html_body){
+            if(error){
+              return cb(error)
+            }
+            const $ = cheerio.load(html_body)
+            const word = $('p').text()
+
+            const wordArray = word.split(" ")
+            cb(null, {url: url,count:wordArray.length})
+          })
+        },
+        callback
+
+      )
+
+
+    }
+}, function(err, result){
+  if(err){
+    throw `Everything is broken ${err}`
+  }
+  console.log(result)
+}
+
+
+)
